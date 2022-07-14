0fc476e5c0b9aa8fa94fe67e03b41401bc649132
Daniel Campos <danielgcampos.04@gmail.com>
2022-07-14 00:43:39 -0300

    add: exception for not file

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+from msilib.schema import Error
 from flask import Flask,request
 import os
 import cv2 
 
 app = Flask(__name__)
 face_recognizer = cv2.face.EigenFaceRecognizer_create()
+face_recognizer.read('modeloEigenFace.xml')
 # face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 # face_recognizer.read('modeloLBPHFace.xml')
-face_recognizer.read('modeloEigenFace.xml')
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
 # ██████████████████████████████████████
@@ -14,11 +15,12 @@ faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalfa
 @app.route('/', methods=['POST'])
 def comprobate__coscu():
     try:
+        if(not request.files.get('file')):
+            raise Exception("No se ha encontrado el archivo")
         coscu = False
         image = request.files['file']
         imageRoute = os.path.join(os.path.dirname(__file__), 'coscu.png') 
         image.save(imageRoute) 
-        
         cap = cv2.VideoCapture('coscu.png')
         ret,frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -36,5 +38,5 @@ def comprobate__coscu():
         cap.release() 
         os.remove(imageRoute)
         return { "IsCoscu": coscu }
-    except:
-        return 'ingrese el archivo' 
+    except Exception as error:
+        return { "error" : str(error) }
